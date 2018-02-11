d7c72b7e9b13fc80d40e9f9dacb945661b9bf4b2
Mitchell Li <msl44@duke.edu>
2018-02-11 14:48:30 -0500

    removed commented tests

--- src/twarkov/TwitterAPI.java
+++ src/twarkov/TwitterAPI.java
@@ -40,29 +40,4 @@ public class TwitterAPI {
 	public String getUser() {
 		return myUsername;
 	}
-
-	
-	/*
-	public static void main(String[] args) throws TwitterException {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		
-		cb.setDebugEnabled(true)
-			.setOAuthConsumerKey("GqF5MVj3CvGPY3JeGHtRAOyK2")
-			.setOAuthConsumerSecret("B1KVzXvLr6gszQAzrwPrh4gPGZBuSpN48zuSV5657OjVilqSyO")
-			.setOAuthAccessToken("962453336585842688-QjRP1VqCMf02Ipe9bOvvBcwofMqfAsw")
-			.setOAuthAccessTokenSecret("gWlbeswOJDrjBG9o3bySclcvY3dhKbtT9d44YONQzsVrV")
-			.setTweetModeExtended(true);
-		
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
-		
-		List<Status> stats = twitter.getUserTimeline("Coder_Coda", new Paging(1,30));
-		//StringBuilder tweets = new StringBuilder();
-		for (Status st:stats) {
-			if (st.getText().substring(0, 4).equals("RT @")) continue; // get rid of retweets
-			//tweets.append(st.getText().replaceAll("https://t.co/\\w*", " ")); // get rid of tweet link
-			System.out.println(st.getText());
-		}
-	}
-	*/
 }
